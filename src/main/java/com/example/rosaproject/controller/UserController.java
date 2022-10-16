5527e5be5266be437bb311791c70d9098f2a2f4e
Mathieu Mariot <math.mariot@gmail.com>
2022-10-16 12:27:39 +0200

    Add header and example html

--- src/main/java/com/example/rosaproject/controller/UserController.java
+++ src/main/java/com/example/rosaproject/controller/UserController.java
@@ -1,4 +1,19 @@
 package com.example.rosaproject.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+
+
+    @GetMapping("/init")
+    public String initExample(){
+        return "example";
+    }
+
+
+
+
+
 }
